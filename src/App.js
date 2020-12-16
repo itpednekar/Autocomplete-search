55831757f4028f7dc322a499111126bcd861c03c
Ishwari T Pednekar <ishwari.pednekar@gslab.com>
2020-12-17 00:17:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Search from "./Search";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} className="App-logo" alt="logo" />
+      <h2>Welcome to React Auto-Complete Search Box!</h2>
+      <div className="app-component">
+        <Search />
+      </div>
     </div>
   );
 }
---
+++ src/Search.js
@@ -0,0 +1,83 @@
+import React, { useState } from "react";
+import "./Search.css";
+
+var FAILURE_COEFF = 10;
+var MAX_SERVER_LATENCY = 200;
+
+function Search() {
+  const [value, setValue] = useState("");
+  const [searchList, setSearchList] = useState([]);
+
+  const onChangeSearch = (e) => {
+    setValue(e.target.value);
+    if (value.length != 0) {
+      getSuggestions(e.target.value)
+        .then((result) => {
+          setSearchList(result);
+        })
+        .catch((err) => {});
+    } else setSearchList([]);
+  };
+
+  const onClickList = (data) => {
+    setValue(data);
+    setSearchList([]);
+  };
+
+  function getRandomBool(n) {
+    var maxRandomCoeff = 1000;
+    if (n > maxRandomCoeff) n = maxRandomCoeff;
+    return Math.floor(Math.random() * maxRandomCoeff) % n === 0;
+  }
+
+  function getSuggestions(text) {
+    var pre = "pre";
+    var post = "post";
+    var results = [];
+    if (getRandomBool(2)) {
+      results.push(pre + text);
+    }
+    if (getRandomBool(2)) {
+      results.push(text);
+    }
+    if (getRandomBool(2)) {
+      results.push(text + post);
+    }
+    if (getRandomBool(2)) {
+      results.push(pre + text + post);
+    }
+    return new Promise((resolve, reject) => {
+      var randomTimeout = Math.random() * MAX_SERVER_LATENCY;
+      setTimeout(() => {
+        if (getRandomBool(FAILURE_COEFF)) {
+          reject();
+        } else {
+          resolve(results);
+        }
+      }, randomTimeout);
+    });
+  }
+
+  return (
+    <div className="autocomplete">
+      <input
+        type="text"
+        placeholder="Search Here.."
+        className="search"
+        onChange={onChangeSearch}
+        value={value}
+      />
+      <ul>
+        {searchList.map((data, index) => {
+          return (
+            value.length != 0 && (
+              <li onClick={() => onClickList(data)}>{data}</li>
+            )
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
+
+export default Search;
